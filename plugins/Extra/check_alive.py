49a661a7e80deb8085f6646001dfabb706cf8ecd
Aliya khan <143610535+ELINA-BOTS@users.noreply.github.com>
2023-12-18 23:20:36 +0530

    Update check_alive.py

--- plugins/Extra/check_alive.py
+++ plugins/Extra/check_alive.py
@@ -10,12 +10,7 @@ CMD = ["/", "."]
 
 @Client.on_message(filters.command("alive", CMD))
 async def check_alive(_, message):
-    await message.reply_text("𝖡𝗎𝖽𝖽𝗒 𝖨 𝖺𝗆 𝖠𝗅𝗂𝗏𝖾 :) 𝖧𝗂𝗍 /start 
-
-𝖧𝗂𝗍 /group_rule 𝖥𝗈𝗋 Group Rules ;)
-
-
-𝖧𝗂𝗍 /ping 𝖳𝗈 𝖢𝗁𝖾𝖼𝗄 𝖡𝗈𝗍 𝖯𝗂𝗇𝗀 😉")
+    await message.reply_text("𝖡𝗎𝖽𝖽𝗒 𝖨 𝖺𝗆 𝖠𝗅𝗂𝗏𝖾 :) 𝖧𝗂𝗍 /start        𝖧𝗂𝗍 /group_rule 𝖥𝗈𝗋 Group Rules.       𝖧𝗂𝗍 /ping 𝖳𝗈 𝖢𝗁𝖾𝖼𝗄 𝖡𝗈𝗍 𝖯𝗂𝗇𝗀 😉")
 
 
 @Client.on_message(filters.command("ping", CMD))
