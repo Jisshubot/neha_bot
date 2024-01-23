f067a6652854cf4da9d9da3b6e797ed4521b7739
Zishan khan <142872501+Jisshubot@users.noreply.github.com>
2024-01-23 15:30:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -106,8 +106,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://neha-jisshubot.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://neha-jisshubot.koyeb.app/".format(FQDN, PORT)
+URL = "https://raspy-eleen-jisshu11.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://raspy-eleen-jisshu11.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -122,9 +122,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://neha-jisshubot.koyeb.app/".format(FQDN)
+    URL = "https://raspy-eleen-jisshu11.koyeb.app/".format(FQDN)
 else:
-    URL = "https://neha-jisshubot.koyeb.app/".format(FQDN)
+    URL = "https://raspy-eleen-jisshu11.koyeb.app/".format(FQDN)
 
 
 
