bf75b37ceb1c23c8d6a71acf6edf2f4474fef96f
Zishan khan <142872501+Jisshubot@users.noreply.github.com>
2024-01-23 15:08:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
-1# Don't Remove Credit @movie_file_20
-2# Subscribe YouTube Channel For Amazing Bot @movie_file_20
-3# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @movie_file_20
+# Subscribe YouTube Channel For Amazing Bot @movie_file_20
+# Ask Doubt on telegram @KingVJ01
 
 
 import re
@@ -34,8 +34,8 @@ PICS_JOIN = (environ.get('PICS_JOIN', 'https://graph.org/file/aafda289da8b859cc2
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5672857559').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001942980809').split()]
-#auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
-#AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
@@ -68,7 +68,7 @@ TUTORIAL = environ.get('TUTORIAL', 'https://t.me/MAPOriginal')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'Hello My Dear Friends ❤️')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001864434358'))
-#SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Movie_fo')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Movie_fo')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
